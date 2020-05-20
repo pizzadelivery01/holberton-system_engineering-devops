e185e447eba80ea47d5ac5357778de67e2e46e9d
Jeremy Bedolla <jjbedolla@gmail.com>
2020-05-20 13:59:46 -0400

    puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix typo in config':
   command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    path  => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     }
